ba5b360e370de19dd2a9071c1c912fac14117385
Matthijs Kooijman <matthijs@stdin.nl>
2022-06-23 23:21:59 +0200

    update.sh: Add script
    This script copies selected files from the STM32CubeWL package (unpacked
    zip or git clone) into this library as needed. It can be used for
    the initial copy as well as for later updates.

---
+++ tools/update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+if [ ! -d "$1" ] || ! grep STM32CubeWL "$1/Package_license.md" 1>/dev/null 2>/dev/null; then
+    echo "No argument passed, or is not an extracted STM32CubeWL package directory"
+    exit 1;
+fi
+CUBE=$1
+ROOT=$(cd $(dirname $0)/.. && pwd)
+DEST="$ROOT/src/STM32CubeWL"
+
+rsync -av "$CUBE/Package_license.md" "$DEST/"
+rsync -av "$CUBE/Middlewares/Third_Party/LoRaWAN" "$DEST/" --exclude '*_template.[ch]' --exclude "/LoRaWAN/LmHandler/*"
+rsync -av "$CUBE/Middlewares/Third_Party/SubGHz_Phy" "$DEST/" --exclude '*_template.[ch]'
+rsync -av "$CUBE/Utilities/timer" --exclude '*_template.[ch]' "$DEST/Utilities"
+rsync -av "$CUBE/Utilities/misc" "$DEST/Utilities" --include '/misc/stm32_systime.[ch]' --include '/misc/*.html' --include '/misc/*.txt' --exclude '/misc/*'
