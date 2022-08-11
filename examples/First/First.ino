3133a1f7a7851d297afda26ad365ddfb106276bb
Matthijs Kooijman <matthijs@stdin.nl>
2022-08-11 05:15:58 +0200

    STM32LoRaWAN: Make class mostly complete
    This is a rather big commit that declares all methods that MKRWAN and
    MKRWAN_v2 have and most (but not all) are implemented yet. Some methods
    will never be implemented, or are complicated so will not be implemented
    soon, these methods are marked with the error attribute to get
    a readable error when used. The methods for channel configuration are
    also missing, but those just need a closer look and an implementation.
    
    This resolves a number of TODOs and makes uplink work using the MKRWAN
    Stream API, makes downlink work and implements most configuration
    methods (and adds a few more). It also adds documentation to most
    methods, though some overall documentation is still needed.
    
    The blocking/non-blocking behavior of join and data transmission is
    still incorrect (to be decided how this shouls work exactly).

--- examples/First/First.ino
+++ examples/First/First.ino
@@ -10,28 +10,31 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println("Start");
-  modem.begin();
+  modem.begin(EU868);
   modem.dataRate(0); // 0 == slowest == most range
 
   modem.joinOTAA(/* AppEui */ "0000000000000000", /* AppKey */ "00000000000000000000000000000000", /* DevEui */ "0000000000000000");
   //modem.joinABP(/* DevAddr */ "00000000", /* NwkSKey */ "00000000000000000000000000000000", /* AppSKey */ "00000000000000000000000000000000");
 
-  while (!modem.connected()) {
-    modem.poll();
-  }
+  modem.maintainUntilIdle();
 
-  Serial.println("Joined");
+  if (modem.connected()) {
+    Serial.println("Joined");
+  } else {
+    Serial.println("Join failed");
+  }
 }
 
 void send_packet()
 {
   uint8_t payload[] = {0xde, 0xad, 0xbe, 0xef};
-  modem.send(payload, sizeof(payload), /* port */ 10, /* confirmed */ false);
+  modem.setPort(10);
+  modem.send(payload, sizeof(payload), /* confirmed */ false);
 }
 
 void loop()
 {
-  modem.poll();
+  modem.maintain();
 
   if (!last_tx || millis() - last_tx > TX_INTERVAL) {
     send_packet();
