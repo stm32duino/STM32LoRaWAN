5e144ab5405fe0afc84ad59bc1ee1dd56f85ecf4
Matthijs Kooijman <matthijs@stdin.nl>
2022-09-12 16:43:58 +0200

    Basic: Improve example
    This removes the maintain calls (which are no longer needed), switches
    to the default blocking packet API, adds a license and adds downlink
    message handling.

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,3 +1,10 @@
+/**
+ * This is a very basic example that demonstrates how to configure the
+ * library, join the network, send regular packets and print any
+ * downlink packets received.
+ *
+ * Revised BSD License - https://spdx.org/licenses/BSD-3-Clause.html
+ */
 #include <STM32LoRaWAN.h>
 #include <BSP/mw_log_conf.h>
 
@@ -13,10 +20,10 @@ void setup()
   modem.begin(EU868);
   modem.dataRate(0); // 0 == slowest == most range
 
+  // Configure join method by (un)commenting the right method
+  // call, and fill in credentials in that method call.
   modem.joinOTAA(/* AppEui */ "0000000000000000", /* AppKey */ "00000000000000000000000000000000", /* DevEui */ "0000000000000000");
   //modem.joinABP(/* DevAddr */ "00000000", /* NwkSKey */ "00000000000000000000000000000000", /* AppSKey */ "00000000000000000000000000000000");
-
-  modem.maintainUntilIdle();
 
   if (modem.connected()) {
     Serial.println("Joined");
@@ -29,13 +36,27 @@ void send_packet()
 {
   uint8_t payload[] = {0xde, 0xad, 0xbe, 0xef};
   modem.setPort(10);
-  modem.send(payload, sizeof(payload), /* confirmed */ false);
+  modem.beginPacket();
+  modem.write(payload, sizeof(payload));
+  modem.endPacket();
+  Serial.println("Sent packet");
+
+  if (modem.available()) {
+    Serial.print("Received packet on port ");
+    Serial.print(modem.getDownlinkPort());
+    Serial.print(": ");
+    while (modem.available()) {
+      uint8_t b = modem.read();
+      Serial.print(b >> 4, HEX);
+      Serial.print(b & 0xF, HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
 }
 
 void loop()
 {
-  modem.maintain();
-
   if (!last_tx || millis() - last_tx > TX_INTERVAL) {
     send_packet();
     last_tx = millis();
