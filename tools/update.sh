ca2eb7c6aa1629b96e7e18b0e0c99670e869ce59
Matthijs Kooijman <matthijs@stdin.nl>
2022-12-14 10:40:27 +0100

    update.sh: Update script for importing from github
    This script used to look at (and copy) the Package_license.md file to
    check that a valid STM32CubeWL directory was passed, but this file is
    only present in zip downloads, not the STM32CubeWL versions released
    through git.
    
    This changes the check to look at package.xml instead, which is present
    in both. It changes the rsync command to copy LICENSE.md, which is only
    present in github releases (so some manual work is needed to import zip
    releases again).

--- tools/update.sh
+++ tools/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-if [ ! -d "$1" ] || ! grep STM32CubeWL "$1/Package_license.md" 1>/dev/null 2>/dev/null; then
+if [ ! -d "$1" ] || ! grep 'Release="FW.WL' "$1/package.xml" 1>/dev/null 2>/dev/null; then
     echo "No argument passed, or is not an extracted STM32CubeWL package directory"
     exit 1;
 fi
@@ -8,7 +8,7 @@ CUBE=$1
 ROOT=$(cd $(dirname $0)/.. && pwd)
 DEST="$ROOT/src/STM32CubeWL"
 
-rsync -av "$CUBE/Package_license.md" "$DEST/"
+rsync -av "$CUBE/LICENSE.md" "$DEST/"
 rsync -av "$CUBE/Middlewares/Third_Party/LoRaWAN" "$DEST/" --exclude '*_template.[ch]' --exclude "/LoRaWAN/LmHandler/*"
 rsync -av "$CUBE/Middlewares/Third_Party/SubGHz_Phy" "$DEST/" --exclude '*_template.[ch]'
 rsync -av "$CUBE/Utilities/timer" --exclude '*_template.[ch]' "$DEST/Utilities"
